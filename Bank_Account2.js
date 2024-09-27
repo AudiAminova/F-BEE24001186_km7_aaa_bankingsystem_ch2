69af972dc67faae346d085613405719c5498bd29
AudiAminova <audiaminovaaini@gmail.com>
2024-09-27 21:38:32 +0700

    file JS challenge sistem banking

---
+++ Bank_Account2.js
@@ -0,0 +1,67 @@
+/* Challange Chapter 2
+program ini berfungsi untuk mensimulasikan fitur dasar perbankan.
+fitur - fitur yang disediakan dalam program ini, yaitu penyetora, penarikan, dan penambahan bunga pada akun tabungan.
+program ini mendefinisikan dua kelas, yaitu BankAccount dan RekeningTabungan.
+class BankAccount digunakan untuk menyimpan data akun tabungan dan memiliki method deposit dan withdraw dari akun, 
+sedangkan class RekeningTabungan yang merupakan turunan dari class BankAccount digunakan untuk mengatur fitur-fitur yang terkait dengan akun tabungan.
+dalam class RekeningTabungan terdapat method tambah_bunga_bank untuk menambah bunga bank ke saldo akun berdasarkan suku bunga dan jumlah bulan.
+cara kerja: user membuat akun dengan menginput nama, no rekening, dan saldo awal. kemudian user diberi opsi 3 pilihan, pilihan 1 untuk deposit,
+pilihan 2 untuk withdraw/penarikan, dan pilihan 3 untuk menghitung bunga bank dengan mengisi suku bungan dan jumlah bulan.
+*/
+
+// class utama/ parent class
+export class BankAccount {
+    constructor(nama, no_rek, saldo) {
+        this.nama = nama;
+        this.no_rek = no_rek;
+        this.saldo = saldo;
+    }
+
+/* method deposit untuk menambahkan saldo ke akun user
+method ini mengambil parameter amount yaitu jumlah uang yang akan disetor */
+deposit(amount) {
+    return new Promise((resolve) => {
+        setTimeout(() => { // proses ini memakan waktu selama 2 detik
+            this.saldo += amount; // amount ditambahkan ke saldo user
+            resolve(`~Akun ${this.nama} telah berhasil melakukan deposit sebesar ${amount}.\nSaldo Anda saat ini adalah ${this.saldo}`); // pesan yang ditampilkan setelah saldo ditambahkan
+        }, 2000); 
+    });
+}
+
+/* method withdraw untuk user menarik uang dari akunnya
+method ini mengambil parameter amount yaitu jumlah yang ingin ditarik */
+withdraw(amount) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => { // proses ini memakan waktu selama 2 detik
+            if (amount > this.saldo) {
+                reject('~Maaf, saldo Anda tidak mencukupi untuk melakukan penarikan'); // jika saldo tidak cukup, pesan ini ditampilkan
+            } else {
+                this.saldo -= amount; // jika saldo cukup, amount dikurangi dari saldo akun user
+                resolve(`~Akun ${this.nama} telah berhasil melakukan penarikan sebesar ${amount}.\nSaldo Anda saat ini adalah ${this.saldo}`); // jika saldo cukup, pesan ini ditampilkan
+            }
+        }, 1000);
+    });
+    }
+}
+
+
+// child class untuk menghitung suku bunga per tahun yang dinyatakan dalam desimal
+export class RekeningTabungan extends BankAccount {
+    constructor(nama, no_rek, saldo, suku_bunga, jmlh_bulan){
+        super(nama, no_rek, saldo); // memanggil constructor parent class
+        this.suku_bunga = suku_bunga; // properti untuk persentase bunga
+        this.jmlh_bulan = jmlh_bulan; // properti untuk menentukan waktu dalam bentuk jumlah bulan
+    }
+
+    // method untuk menambahkan bunga ke saldo user berdasarkan perhitungan suku bunga dan jumlah bulan
+    tambah_bunga_bank() {
+        return new Promise((resolve) => {
+            setTimeout(() => { // proses ini memakan waktu selama 2 detik
+                const bunga_bank = this.saldo * (this.suku_bunga / 100) * (this.jmlh_bulan / 12); // rumus menghitung bunga
+                this.saldo += parseFloat(bunga_bank.toFixed(2)); // menambahkan saldo dengan hasil perhitungan bunga dengan ditetapkan 2 angka dibelakang koma
+                resolve(`~Bunga sebesar ${bunga_bank.toFixed(2)} telah ditambahkan ke akun ${this.nama}.\nSaldo Anda saat ini ${this.saldo}`);
+            }, 2000);
+        });
+    }
+}
+
